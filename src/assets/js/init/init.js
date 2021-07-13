0dadac0fb1442ca7c0758c24fc604c9eddb69c03
Pavel Pavlov <pp@softevol.com>
2021-07-13 21:20:29 +0300

    add filter slider

---
+++ src/assets/js/init/init.js
@@ -0,0 +1,9 @@
+$(document).ready(function () {
+    $('.slick-carousel').slick({
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        arrows: false,
+        autoplay: true,
+        autoplaySpeed: 2000,
+    });
+});

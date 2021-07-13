f40d0172d202be38e0a1bb78cd4b1a28fb5946b3
Pavel Pavlov <pp@softevol.com>
2021-07-13 19:15:33 +0300

    add top menu

--- src/_data/i18n/index.js
+++ src/_data/i18n/index.js
@@ -1,30 +1,34 @@
 module.exports = {
-    hello: {
-      'en-GB': 'Hello',
-      'ru-RU': 'Привет'
-    },
-    select_language: {
-      'en-GB': 'Select language',
-      'ru-RU': 'Выберите язык'
-    },
-    home: {
-      'en-GB': 'home',
-      'ru-RU': 'главная'
-    },
-    typography: {
-      'en-GB': 'typography',
-      'ru-RU': 'типография'
-    },
-    ui_kit: {
-      'en-GB': 'ui-kit',
-      'ru-RU': 'ui-kit'
-    },
-    picture: {
-      'en-GB': 'picture',
-      'ru-RU': 'picture'
-    },
-    color_system: {
-      'en-GB': 'color system',
-      'ru-RU': 'цветовая схема'
-    },
+  hello: {
+    'en-GB': 'Hello',
+    'ru-RU': 'Привет'
+  },
+  select_language: {
+    'en-GB': 'Select language',
+    'ru-RU': 'Выберите язык'
+  },
+  home: {
+    'en-GB': 'Home',
+    'ru-RU': 'Главная'
+  },
+  about: {
+    'en-GB': 'About',
+    'ru-RU': 'О нас'
+  },
+  services: {
+    'en-GB': 'Services',
+    'ru-RU': 'Услуги'
+  },
+  portfolio: {
+    'en-GB': 'Portfolio',
+    'ru-RU': 'Портфолио'
+  },
+  clients: {
+    'en-GB': 'Clients',
+    'ru-RU': 'Клиенты'
+  },
+  contact: {
+    'en-GB': 'Contact',
+    'ru-RU': 'Контакты'
+  },
 };
